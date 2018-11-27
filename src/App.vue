fbc4bc0b9eb9fbe3aea15fbe2d183b196c3b0e16
Angel Aguilera <albertoangelaguilera@gmail.com>
2018-11-26 21:29:00 -0600

    Propio sistema de administracion escolar del IPN

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+font-family: 'Quicksand', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Materias.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+    <md-table>
+      <md-table-row>
+        <md-table-head>Codigo Materia</md-table-head>
+        <md-table-head>Materia</md-table-head>
+        <md-table-head>Profesor</md-table-head>
+        <md-table-head>Area</md-table-head>
+      </md-table-row>
+      <md-table-row v-for="materia in materias">
+        <md-table-cell><p>{{materia.id}}</p></md-table-cell>
+        <md-table-cell>{{materia.Name}}</md-table-cell>
+        <md-table-cell>{{materia.Sem1.Teacher.Name}} {{materia.Sem1.Teacher.LastN}}</md-table-cell>
+        <md-table-cell>{{materia.Area}}</md-table-cell>
+      </md-table-row>
+    </md-table>
+  </div>
+  </div>
+</template>
+<script>
+import { db } from '../main'
+
+export default {
+     data() {
+    return {
+      materias:[
+         
+      ]    }
+  },
+  firestore(){
+      return{
+      materias:db.collection("Subjects").where("Area","==","Ingenieria")
+  }
+  },
+  mounted(){
+  }
+}
+</script>
+<style>
+
+</style>
